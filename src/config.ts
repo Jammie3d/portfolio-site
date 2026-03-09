fd0ce8eb1bfe0c1e731a396f817c7337deb1dd72
Jammie3d <60801006+Jammie3d@users.noreply.github.com>
2026-03-09 19:31:43 +0000

    Projects progress

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jay Morgan - 3D Artist';
-export const SITE_DESCRIPTION = 'Portfolio website for Jay Morgan, the 3D artist. Showcasing a list of projects.';
+export const SITE_TITLE = 'Jay Morgan';
+export const SITE_DESCRIPTION = 'Portfolio website for Jay Morgan, the 3D environment artist.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
